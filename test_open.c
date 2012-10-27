d7ef738b1cfe62d4ea843b2459b06f53046253a8
root <root@rharrison33-U43F.(none)>
2012-10-27 17:34:22 -0400

    Added timer to test_open.c

--- test_open.c
+++ test_open.c
@@ -4,6 +4,11 @@ int main() {
 	
 	FILE *myfile;
     char tempstring[1024];
+    
+    struct timespec tp_start;
+	if(clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &tp_start) < 0){
+		printf("clock_gettime() failed\n");
+	}
 
     if(!(myfile=fopen("/nethome/hchan35/source_code/cs3210_proj2/README.md","r")))
     {
@@ -17,5 +22,13 @@ int main() {
          printf("%s",tempstring);
     }
 	printf("\n");
+	
+	struct timespec tp_end;
+	if(clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &tp_end) < 0){
+		printf("clock_gettime() failed\n");
+	}
+	struct timespec tp_diff = diff(timespec start, timespec end);
+	std::cout << "This test took " << tp_diff.tv_sec << " seconds" << endl;
+	
 	return 0;
 }
