c8ee969ae80b052fd09b827b98571817c2300e7a
Ben Chan <ustbenchan@gmail.com>
2012-10-31 20:11:43 -0400

    update pull log method

--- syscallLogPull.c
+++ syscallLogPull.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
 
-#define procfs_name 
+#define procfs_name "syslog"
 
 int main() {
-	
+
 	FILE *procFile, *outputFile;
-    char tempstring[1024];
+	char tempstring[3][1024];
 
-    if(!(procFile=fopen("/proc/syslog","r")))
-    {
-         fprintf(stderr,"Could not open file\n");
-         return -1;
-    }
+	if(!(procFile=fopen("/proc/syslog","r")))
+	{
+		fprintf(stderr,"Could not open file\n");
+		return -1;
+	}
 
-    while(!feof(procFile))
-    {
-         fscanf(procFile,"%s",tempstring);
-         printf("%s\n",tempstring);
-    }
-	printf("\n");
-	
+	while(!feof(procFile))
+	{
+		fscanf(procFile,"%s",tempstring[0]);
+		printf("%s ",tempstring[0]);
+		
+		fscanf(procFile,"%s",tempstring[1]);
+		printf("%s ",tempstring[1]);
+		
+		fscanf(procFile,"%s",tempstring[2]);
+		printf("%s\n",tempstring[2]);
+		
+	}
+	printf("---End---\n");
+
 	return 0;
 }
